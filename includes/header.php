877830ecc5263db4f26af338a8bb1a74e182257b
fjfjeronimo <46676204+fjfjeronimo@users.noreply.github.com>
2019-01-28 00:07:55 +0000

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $titulo ?></title>
+    <link rel="icon" type="image/x-icon" href="media/favicon.ico" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="h-left">
+            <h1><?php echo $t_header ?></h1>
+        </div>
+        <div class="h-right">
+            <ul>
+                <a href="index.php">HTML</a>
+                <a href="css.php">CSS</a>
+                <a href="js.php">JS</a>
+                <a href="php.php">PHP</a>
+                <a href="mysql.php">MYSQL</a>
+            </ul>
+        </div>
+    </header>
---
+++ js/main.js
@@ -0,0 +1,111 @@
+$('#rp h2').on('click', () => {
+    if ($("#rp .snip").is(":visible")) {
+        $("#rp .snip").slideUp(300);
+    } else {
+        $("#rp .snip").slideDown(300);
+    }
+});
+
+$('#md h2').on('click', () => {
+    if ($("#md .snip").is(":visible")) {
+        $("#md .snip").slideUp(300);
+    } else {
+        $("#md .snip").slideDown(300);
+    }
+});
+
+$('#rs h2').on('click', () => {
+    if ($("#rs .snip").is(":visible")) {
+        $("#rs .snip").slideUp(300);
+    } else {
+        $("#rs .snip").slideDown(300);
+    }
+});
+
+$('#sc h2').on('click', () => {
+    if ($("#sc .snip").is(":visible")) {
+        $("#sc .snip").slideUp(300);
+    } else {
+        $("#sc .snip").slideDown(300);
+    }
+});
+
+$('#ct h2').on('click', () => {
+    if ($("#ct .snip").is(":visible")) {
+        $("#ct .snip").slideUp(300);
+    } else {
+        $("#ct .snip").slideDown(300);
+    }
+});
+
+$('#ste h2').on('click', () => {
+    if ($("#ste .snip").is(":visible")) {
+        $("#ste .snip").slideUp(300);
+    } else {
+        $("#ste .snip").slideDown(300);
+    }
+});
+
+$('#im h2').on('click', () => {
+    if ($("#im .snip").is(":visible")) {
+        $("#im .snip").slideUp(300);
+    } else {
+        $("#im .snip").slideDown(300);
+    }
+});
+
+$('#ci h2').on('click', () => {
+    if ($("#ci .snip").is(":visible")) {
+        $("#ci .snip").slideUp(300);
+    } else {
+        $("#ci .snip").slideDown(300);
+    }
+});
+
+$('#scc h2').on('click', () => {
+    if ($("#scc .snip").is(":visible")) {
+        $("#scc .snip").slideUp(300);
+    } else {
+        $("#scc .snip").slideDown(300);
+    }
+});
+
+$('#de h2').on('click', () => {
+    if ($("#de .snip").is(":visible")) {
+        $("#de .snip").slideUp(300);
+    } else {
+        $("#de .snip").slideDown(300);
+    }
+});
+
+$('#ctt h2').on('click', () => {
+    if ($("#ctt .snip").is(":visible")) {
+        $("#ctt .snip").slideUp(300);
+    } else {
+        $("#ctt .snip").slideDown(300);
+    }
+});
+
+$('#fo h2').on('click', () => {
+    if ($("#fo .snip").is(":visible")) {
+        $("#fo .snip").slideUp(300);
+    } else {
+        $("#fo .snip").slideDown(300);
+    }
+});
+
+$('#ei h2').on('click', () => {
+    if ($("#ei .snip").is(":visible")) {
+        $("#ei .snip").slideUp(300);
+    } else {
+        $("#ei .snip").slideDown(300);
+    }
+});
+
+$('#cw h2').on('click', () => {
+    if ($("#cw .snip").is(":visible")) {
+        $("#cw .snip").slideUp(300);
+    } else {
+        $("#cw .snip").slideDown(300);
+    }
+});
